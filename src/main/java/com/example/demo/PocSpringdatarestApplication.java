31227992371a03fc3a902d92a2021e21d8434166
jenith <jenithleon@live.com>
2018-08-11 00:44:09 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/PocSpringdatarestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringdatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringdatarestApplication.class, args);
+	}
+}
